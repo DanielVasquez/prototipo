79c247331dfaa7e02e684acdfc44f573a2701be2
Daniel Vasquez <danielvp1987@gmail.com>
2014-05-11 15:22:11 -0500

    Controlador para paginas estaticas

---
+++ app/controllers/paginas_estaticas_controller.rb
@@ -0,0 +1,10 @@
+class PaginasEstaticasController < ApplicationController
+  def perfil
+  end
+
+  def liga
+  end
+
+  def equipo
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'paginas_estaticas/perfil'
+
+  get 'paginas_estaticas/liga'
+
+  get 'paginas_estaticas/equipo'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
